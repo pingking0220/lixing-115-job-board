5fc438cf364d73ab0fffda66108a3946f2e5ae97
pingking0220 <pingking0220@users.noreply.github.com>
2026-05-06 08:58:38 +0800

    Connect Firebase realtime database

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 window.LIXING_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCI5pI3Sb_7y7ltn_QH2A2NKGDPulrb6Zs",
+  authDomain: "lixing-115-job-board.firebaseapp.com",
+  databaseURL: "https://lixing-115-job-board-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lixing-115-job-board",
+  storageBucket: "lixing-115-job-board.firebasestorage.app",
+  messagingSenderId: "834411365685",
+  appId: "1:834411365685:web:1f6836082807ff10fe787c"
 };
 
 window.LIXING_DATABASE_PATH = "boards/lixing-115/jobs";
